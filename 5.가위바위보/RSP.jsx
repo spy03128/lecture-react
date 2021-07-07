69bb07caba6be4b7b62df515862b91810a068ffb
지현 <spy03128@naver.com>
2021-07-07 20:14:55 +0900

    setInterval과라이프사이클 연동

--- 5.가위바위보/RSP.jsx
+++ 5.가위바위보/RSP.jsx
@@ -5,23 +5,60 @@ import React, { Component } from "react";
 //(setState/props 바뀔때) -> render -> componentDidUpdate
 //부모가 나를 없앨 때 -> componentWillUnmount -> 소멸
 
+const rspCoords = {
+  바위: "0",
+  가위: "-142px",
+  보: "-284px",
+};
+
+const scores = {
+  가위: 1,
+  바위: 0,
+  보: -1,
+};
+
 class RSP extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    imgCoord: rspCoords.바위,
     score: 0,
   };
 
+  interval;
+
   //처음 render가 성공적으로 실행된다면 componentDidMount가 실행됨.
   //비동기 요청을 많이 함
-  componentDidMount() {}
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      const { imgCoord } = this.state;
+
+      if (imgCoord === rspCoords.바위) {
+        this.setState({
+          imgCoord: rspCoords.가위,
+        });
+      } else if (imgCoord === rspCoords.가위) {
+        this.setState({
+          imgCoord: rspCoords.보,
+        });
+      }
+      if (imgCoord === rspCoords.보) {
+        this.setState({
+          imgCoord: rspCoords.바위,
+        });
+      }
+    }, 1000);
+  }
 
   //re rendering 후에 실행
   componentDidUpdate() {}
 
   //component가 제거되기 직전에 실행됨.
   //비동기 요청 정리를 함
-  componentWillUnmount() {}
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  onClickBtn = (choice) => {};
 
   render() {
     const { result, score, imgCoord } = this.state;
@@ -34,17 +71,25 @@ class RSP extends Component {
           }}
         />
         <div>
-          <button id="rock" className="btn" onClick={() => onClickBtn("바위")}>
+          <button
+            id="rock"
+            className="btn"
+            onClick={() => this.onClickBtn("바위")}
+          >
             바위
           </button>
           <button
             id="scissor"
             className="btn"
-            onClick={() => onClickBtn("가위")}
+            onClick={() => this.onClickBtn("가위")}
           >
             가위
           </button>
-          <button id="paper" className="btn" onClick={() => onClickBtn("보")}>
+          <button
+            id="paper"
+            className="btn"
+            onClick={() => this.onClickBtn("보")}
+          >
             보
           </button>
         </div>
